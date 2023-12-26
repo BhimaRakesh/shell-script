ceb194da8ee184c2a45ffeccf8121106cc6c5d64
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-26 12:18:31 +0530

    as part of bug fixing

---
+++ display.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "hi rakesh how are you"
+echo "fine ravi what about you"
+echo "yaa fine what are you doing Rakesh"
+echo "iam learning Devops course ravi"
+echo "ho ok ,how much cost rakesh"
