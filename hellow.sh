0d97c4d8bfec9d5c2be5e9729425f7eb9d39f48b
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-26 13:48:40 +0530

    issue resolved

--- hellow.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "hi rakesh how are you"
-echo "fine ravi what about you"
-echo "yaa fine what are you doing Rakesh"
-echo "iam learning Devops course ravi"
-echo "ho ok ,how much cost rakesh"
