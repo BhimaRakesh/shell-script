f9f3e6bc7c3adb452063d306d471e859e6774ac6
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-26 16:16:19 +0530

    arguments use

--- arguments.sh
+++ arguments.sh
@@ -1,8 +1,12 @@
-person1=ramu
-person2=raghu
+person1=$1
+person2=$2
 
-echo "$1: hi $2 how are you"
-echo "$2: fine $1 what about you"
-echo "$1 : yaa fine what are you doing $2"
-echo "$1: iam learning Devops course $1"
-echo "$2 : ho ok ,how much cost $2"
+echo " $person1: hi $person2 how are you"
+echo " $person2: fine $person1 what about you"
+echo "$person1 : yaa fine what are you doing $person2"
+echo "$person2: iam learning Devops course $person1"
+echo "$person1 : ho ok ,how much cost $person2"
+
+
+echo "in this file we  nedd to write or pass the arguments like the person names 
+when we are print in server like sh filename.sh person name1 person name2 "
