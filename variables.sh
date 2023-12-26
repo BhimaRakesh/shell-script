efb280aa4667eb808584988bb0faa84c7eab7864
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-26 14:17:15 +0530

    i file added

---
+++ variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "ravi : hi rakesh how are you"
+echo "rakesh : fine ravi what about you"
+echo "ravi : yaa fine what are you doing Rakesh"
+echo "rakesh : iam learning Devops course ravi"
+echo "ravi : ho ok ,how much cost rakesh"
+
+echo "we have repeted values like rakesh,ravi ryt so 
+we need to decrease thisrepeated words because sometimes we want to change 
+rakesh instance rahul so we need lot of time to consume so 
+use variables to store or hold the data or words"
+
+person1=rakesh
+person2=raghu
+
+echo " $person1: hi $person2 how are you"
+echo " $person2: fine$person1 what about you"
+echo "$person1 : yaa fine what are you doing $person2"
+echo "$person2: iam learning Devops course $person1"
+echo "$person1 : ho ok ,how much cost $person2"
