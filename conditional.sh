27e692f1b670d35bfa940a0419ef99da3df2bac3
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-28 20:59:10 +0530

    2

--- conditional.sh
+++ conditional.sh
@@ -2,7 +2,7 @@
 
 #ID=$(id -u)
 
-#if[ 1 -ne 0 ]
+#if[ 1 != 0 ]
 #then
 #echo "your not a root user"
 #exit 1
@@ -15,8 +15,10 @@ b=20
   
 #Check whether they are equal 
 if [ $a == $b ] 
-then 
-    echo "a is equal to b"
+then   
+echo "a is equal to b"
+else 
+echo "hahaha"
 fi 
   
 #Check whether they are not equal 
