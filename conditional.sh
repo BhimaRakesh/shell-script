cd24ddb4d7331c7beb61396d2f5e3c24b70da4fd
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-28 21:40:34 +0530

    5

--- conditional.sh
+++ conditional.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+R="\e[31m"
+N="\e[0m"
+G="\e[32m"
 
 ID=$(id -u)
 
-if[ $a != 0 ]
-#if [ $ID != 0 ] 
+
+if [ $ID != 0 ] 
 then
    echo "your not a root user"
 else
    echo "your a root user"
 fi
+
+yum install mysql -y
+
+if [$?  != 0 ]
+then
+ echo -e "installation is $R failed $N"
+ else
+ echo "installation is  $G completed $N"
