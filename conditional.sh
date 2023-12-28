689c162c6c0006205e6ec9868ac8f50ad71a3e41
bhima rakesh <brakeshrakesh5@gmail.comgit commit -m bug>
2023-12-29 00:24:34 +0530

    try to fix

--- conditional.sh
+++ conditional.sh
@@ -3,10 +3,18 @@
 R="\e[31m"
 N="\e[0m"
 G="\e[32m"
-
+Y="\e[33m"
 ID=$(id -u)
 
 
+VALIDATE(){
+    if [ $1 != 0 ]
+    then
+    echo -e "$2..$R failed"
+    else
+    echo -e "$2..$G success $N"
+    fi
+}
 if [ $ID != 0 ] 
 then
    echo "your not a root user"
@@ -14,11 +22,16 @@ else
    echo "your a root user"
 fi
 
-yum install mysql -y
+for package in $@
+do 
+yum list installed $package
 
 if [ $? != 0 ]
 then
- echo -e "installation is $R failed "
+yum  install $package -y
+
+VALIDATE $? "installation of $package "
 else
- echo -e "installation is $G completed "
+
+ echo -e "$package is already installed $Y skipping"
 fi
